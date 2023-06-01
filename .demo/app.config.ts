934ad7bfe10c2f12b76f451bfd7bf51a8430f935
Daniel van der merwe <daniel@rokkit200.co>
2023-06-01 19:01:40 +0200

    initial commit

---
+++ .demo/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Elysium here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: 'vandermerwed',
+    github: 'vandermerwed/elysium'
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
---
+++ app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+  socials: {
+    twitter: '',
+    github: ''
+  },
+  nuxtIcon: {
+    aliases: {
+      'dark-mode': 'ph:moon-bold',
+      'light-mode': 'ph:sun-bold'
+    }
+  }
+})
