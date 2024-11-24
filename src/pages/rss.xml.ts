8aeab29439a9ecc4173c68a9e457954e5d15f66f
Daniel van der Merwe <daniel@rokkit200.co>
2024-11-24 08:33:01 +0200

    feat: add markdown parsing and sanitization to RSS feed generation

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,9 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { getSortedPosts } from "@utils/getSortedPosts";
 import { SITE } from "@config";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET() {
   const posts = await getCollection("blog");
@@ -10,11 +13,14 @@ export async function GET() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data, slug }) => ({
-      link: `notes/${slug}/`,
+    items: sortedPosts.map(({ data, body, slug }) => ({
+      link: `notes/${slug}`,
       title: data.title,
       description: data.description,
-      pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(body)),
+      categories: data.tags,
+      pubDate: new Date(data.pubDatetime),
     })),
   });
 }
